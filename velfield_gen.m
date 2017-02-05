fd8ad68d0ef6f3a63fe7ab21d3c9e73afd99d17c
Matthew Petersen <matthew_petersen@brown.edu>
2017-02-05 12:13:23 -0500

    first commit

---
+++ velfield_gen.m
@@ -0,0 +1,16 @@
+function velfield_gen
+
+%% Load in data (depends on FIDIC output format). Output of section: cell vectors U and V with X and Y displacement matrices. 
+
+%% Generate instantaneous velocity matrix cell vectors P and Q for X and Y 
+
+%% Collect circle data
+
+%% Create location grids - use circle data to scale based on radius
+
+%% Convert matrices to one of [x y u v]
+
+%% Convert velocity to polar - output [r theta rho], can throw away radial velocity
+
+%% loop for spatial averaging inside loop for time averaging
+end
